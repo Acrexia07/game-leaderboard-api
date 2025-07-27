6da01c877a585698e2ce43a40569a848395a0ee7
Acrexia07 <barnuevo.marlon07@gmail.com>
2025-07-27 23:09:40 +0800

    Initial commit: Spring Boot setup via Initializr, CI workflow, and dummy test for initial build verification

---
+++ src/test/java/com/marlonb/game_leaderboard_api/DummyTest.java
@@ -0,0 +1,13 @@
+package com.marlonb.game_leaderboard_api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DummyTest {
+
+    @Test
+    void alwaysPasses(){
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/com/marlonb/game_leaderboard_api/GameLeaderboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlonb.game_leaderboard_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLeaderboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
