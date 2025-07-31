67eedfab94f7ebb5b44fb453e8cd7c7b171a313d
Acrexia07 <barnuevo.marlon07@gmail.com>
2025-07-31 16:46:50 +0800

    chore: add repository file for player <PlayerRepository.java>

---
+++ src/main/java/com/marlonb/game_leaderboard_api/repository/PlayerRepository.java
@@ -0,0 +1,9 @@
+package com.marlonb.game_leaderboard_api.repository;
+
+import com.marlonb.game_leaderboard_api.model.PlayerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
+}
