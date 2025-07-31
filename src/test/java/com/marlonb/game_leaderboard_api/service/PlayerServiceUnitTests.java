9f798d1d77de1807e04fb036067677ca04ac5d49
Acrexia07 <barnuevo.marlon07@gmail.com>
2025-07-31 18:22:43 +0800

    Fix: add active profile annotation in the test

--- src/test/java/com/marlonb/game_leaderboard_api/service/PlayerServiceUnitTests.java
+++ src/test/java/com/marlonb/game_leaderboard_api/service/PlayerServiceUnitTests.java
@@ -14,12 +14,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-@Slf4j
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
 public class PlayerServiceUnitTests {
 
     @Mock
