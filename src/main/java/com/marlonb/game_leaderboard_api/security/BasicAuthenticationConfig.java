8a6651e34d7e3383ba7bfb4e61ed1d858881dff6
Acrexia07 <barnuevo.marlon07@gmail.com>
2025-08-06 17:13:11 +0800

    feat: add basic authentication as initial spring security

---
+++ src/main/java/com/marlonb/game_leaderboard_api/security/BasicAuthenticationConfig.java
@@ -0,0 +1,52 @@
+package com.marlonb.game_leaderboard_api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.AbstractConfiguredSecurityBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class BasicAuthenticationConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws  Exception {
+
+        return http.authorizeHttpRequests(
+                        auth -> auth.requestMatchers("/api/**")
+                                .hasRole("ADMIN")
+                                .anyRequest()
+                                .permitAll())
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .httpBasic(Customizer.withDefaults())
+                    .sessionManagement(session ->
+                                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService () {
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                    .username("acrexia")
+                    .password(passwordEncoder().encode("dummy"))
+                    .authorities("ROLE_ADMIN")
+                    .build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
+}
