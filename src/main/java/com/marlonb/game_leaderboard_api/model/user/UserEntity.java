8b49bf2a9a11b539c17a886ec15fd18746994322
Acrexia07 <barnuevo.marlon07@gmail.com>
2025-08-23 01:53:44 +0800

    feat: add new entity for JWT implementation preparation

---
+++ src/main/java/com/marlonb/game_leaderboard_api/model/user/UserEntity.java
@@ -0,0 +1,36 @@
+package com.marlonb.game_leaderboard_api.model.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users_data")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRoles role = UserRoles.PLAYER;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate () {
+        this.createdAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/marlonb/game_leaderboard_api/model/user/UserRoles.java
@@ -0,0 +1,5 @@
+package com.marlonb.game_leaderboard_api.model.user;
+
+public enum UserRoles {
+    PLAYER, ADMIN
+}
