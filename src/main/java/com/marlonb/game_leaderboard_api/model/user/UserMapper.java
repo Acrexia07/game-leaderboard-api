5de8586007f5119ec0c7e8c7d8d36a9f8c8c106c
Acrexia07 <barnuevo.marlon07@gmail.com>
2025-08-24 15:25:25 +0800

    feat: add mapper for user entity's dtos

---
+++ src/main/java/com/marlonb/game_leaderboard_api/model/user/UserMapper.java
@@ -0,0 +1,19 @@
+package com.marlonb.game_leaderboard_api.model.user;
+
+import org.mapstruct.*;
+
+@Mapper(
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL
+)
+public interface UserMapper {
+
+    UserResponseDto toResponse (UserEntity userResponse);
+
+    @Mapping(target = "id", ignore = true)
+    UserEntity toEntity (UserRequestDto userRequest);
+
+    @Mapping(target = "id", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void toUpdateFromEntity (@MappingTarget UserEntity user, UserUpdateDto userUpdate);
+}
